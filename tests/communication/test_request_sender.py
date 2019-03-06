d9ff34053390c1e1589b31382b0db57cf86c165e
dev1-fund3 <dev@fund3.co>
2019-03-06 12:42:21 -0800

    cleaning up test_request_sender

--- tests/communication/test_request_sender.py
+++ tests/communication/test_request_sender.py
@@ -124,14 +124,16 @@ def test_message_sending_to_router(fake_router_socket,
 def test_place_order(fake_request_sender):
     order = Order(
         account_info=AccountInfo(account_id=100),
-        client_order_id=8675309,
+        client_order_id=str(8675309),
         client_order_link_id='a123',
         symbol='BTC/USD',
         side=Side.buy.name,
         order_type=OrderType.limit.name,
         quantity=1.1,
         price=6000.01,
+        stop_price=0.0,
         time_in_force=TimeInForce.gtc.name,
+        expire_at=0.0,
         leverage_type=LeverageType.none.name
     )
     order = fake_request_sender.place_order(
@@ -143,7 +145,9 @@ def test_place_order(fake_request_sender):
     assert order.orderType == 'limit'
     assert order.quantity == 1.1
     assert order.price == 6000.01
+    assert order.stop_price == 0.0
     assert order.timeInForce == 'gtc'
+    assert order.expire_at == 0.0
 
 
 @pytest.mark.test_id(4)
@@ -155,7 +159,9 @@ def test_replace_order(fake_request_sender):
         quantity=1.1,
         order_type=OrderType.limit.name,
         price=6000.01,
-        time_in_force=TimeInForce.gtc.name
+        stop_price=0.0,
+        time_in_force=TimeInForce.gtc.name,
+        expire_at=0.0
     )
     assert type(order) == capnp.lib.capnp._DynamicStructBuilder
     assert order.accountInfo.accountID == 100
@@ -163,7 +169,9 @@ def test_replace_order(fake_request_sender):
     assert order.orderType == 'limit'
     assert order.quantity == 1.1
     assert order.price == 6000.01
+    assert order.stop_price == 0.0
     assert order.timeInForce == 'gtc'
+    assert order.expire_at == 0.0
 
 
 @pytest.mark.test_id(5)
@@ -382,7 +390,7 @@ def test_request_completed_orders(fake_request_sender):
 def test_place_order_margin_default(fake_request_sender):
     default_margin_order = Order(
         account_info=AccountInfo(account_id=100),
-        client_order_id=9876,
+        client_order_id=str(9876),
         client_order_link_id='a123',
         symbol='BTC/USD',
         side=Side.buy.name,
@@ -412,9 +420,9 @@ def test_place_order_margin_default(fake_request_sender):
 def test_place_order_margin_custom(fake_request_sender):
     custom_margin_order = Order(
         account_info=AccountInfo(account_id=100),
-        client_order_id=9876,
+        client_order_id=str(9876),
         client_order_link_id='a123',
-        symbol='BTC/USD',
+        symbol='BTC/USD',t
         side=Side.buy.name,
         order_type=OrderType.market.name,
         quantity=1.1,
