d4aebe4767f1df2c6185ea60dac794d9355efd90
dev1-fund3 <dev@fund3.co>
2019-05-06 15:32:29 -0700

    updated SingleClientSessionRefresher example

--- omega_client/examples/single_client_session_refresher.py
+++ omega_client/examples/single_client_session_refresher.py
@@ -1,22 +1,22 @@
+import logging
 import time
 from datetime import datetime as dt
 from threading import Thread, Event
 
-from omega_client.communication.request_sender import RequestSender
+from omega_client.communication.omega_connection import OmegaConnection
 from omega_client.messaging.common_types import RequestHeader, \
     AuthorizationRefresh, AuthorizationGrant
+
+logger = logging.getLogger(__name__)
 
 
 class SingleClientSessionRefresher(Thread):
     def __init__(self,
-                 request_sender: RequestSender,
+                 request_sender: OmegaConnection,
                  client_id: int,
                  sender_comp_id: str):
         """
-        This is an example class used for refreshing the session of a single
-        client (1 client_id). If you have multiple users, you need to develop
-        your a method on your own for refreshing the session for multiple users.
-        This class is used by the logon_logoff.py example.
+        Refreshes the session of a single client (1 client_id).
 
         :param request_sender: RequestSender class used for sending messages
         to Omega
@@ -33,7 +33,7 @@ class SingleClientSessionRefresher(Thread):
         self.sender_comp_id = sender_comp_id
         self.request_id = 0
 
-        self.waiting_for_new_token = False
+        self.waiting_for_new_token = Event()
 
         self._is_running = Event()
 
@@ -50,35 +50,30 @@ class SingleClientSessionRefresher(Thread):
         """
         self._is_running.set()
         while self.is_running():
-            # sleep until 10 seconds before the token expires
+            # sleep until 100 seconds before the token expires
             time_until_session_refresh = (self.token_expire_time -
-                                          dt.utcnow().timestamp() - 10.)
-            print('SessionRefresher sleeping {} seconds'.format(
+                                          dt.utcnow().timestamp() - 100.)
+            logger.info('SessionRefresher sleeping {} seconds'.format(
                 time_until_session_refresh))
-            time.sleep(seconds=time_until_session_refresh)
+            time.sleep(time_until_session_refresh)
 
-            # increment the request_id
             self.request_id += 1
 
-            # send the authorization refresh request to Omega
-            self.request_sender.request_authorization_refresh(
-                request_header=RequestHeader(
-                    client_id=self.client_id,
-                    sender_comp_id=self.sender_comp_id,
-                    access_token=self.access_token,
-                    request_id=self.request_id
-                ),
-                auth_refresh=AuthorizationRefresh(
-                    refresh_token=self.refresh_token)
-            )
-
-            self.waiting_for_new_token = True
-
-            # poll every second waiting for AuthorizationGrant Response to
-            # update token
-            while self.waiting_for_new_token:
-                print('SessionRefresher waiting 1 second for new token')
-                time.sleep(1)
+            self.waiting_for_new_token.clear()
+            while not self.waiting_for_new_token.is_set():
+                # send the authorization refresh request to Omega
+                self.request_sender.request_authorization_refresh(
+                    request_header=RequestHeader(
+                        client_id=self.client_id,
+                        sender_comp_id=self.sender_comp_id,
+                        access_token=self.access_token,
+                        request_id=self.request_id
+                    ),
+                    auth_refresh=AuthorizationRefresh(
+                        refresh_token=self.refresh_token)
+                )
+                self.waiting_for_new_token.wait(timeout=1)
+                logger.info('waited for 1 seconds for new token')
 
         return
 
@@ -93,10 +88,11 @@ class SingleClientSessionRefresher(Thread):
             self.access_token = str(auth_grant.access_token)
             self.refresh_token = str(auth_grant.refresh_token)
             self.token_expire_time = float(auth_grant.expire_at)
-            self.waiting_for_new_token = False
-            print('SessionRefresher successfully updated access token')
+            self.request_sender.update_access_token(self.access_token)
+            self.waiting_for_new_token.set()
+            logger.info('SessionRefresher successfully updated access token')
             return True
-        print('SessionRefresher failed to successfully update access token. '
-              'Stopping.')
+        logger.info('SessionRefresher failed to successfully update access '
+                    'token. Stopping.')
         self.stop()
         return False
